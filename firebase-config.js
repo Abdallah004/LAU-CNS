e374cb13e18cb05fcf90d4351d03261b54619298
Abdallah004 <abdallahmeraachli@gmail.com>
2024-12-15 15:02:42 +0200

    UPDATE CONFIG SERVER SIDE

--- firebase-config.js
+++ firebase-config.js
@@ -2,16 +2,15 @@
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCQV_GoKqsgcPJwuM5j0XlOWkXRA7L81CM",
-    authDomain: "lau-cns.firebaseapp.com",
-    databaseURL: "https://lau-cns-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "lau-cns",
-    storageBucket: "lau-cns.firebasestorage.app",
-    messagingSenderId: "1002579331045",
-    appId: "1:1002579331045:web:87d20ecd9bc5f637fa110c",
-    measurementId: "G-JLT33CTQ9X"
-  };
-  
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
   // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
   const auth = firebase.auth();
